05b0607291ed349f1c07a217e28af2f035525e2c
zhaofeng-shu33 <616545598@qq.com>
2020-09-13 11:49:27 +0800

    move to right dir

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 # system level
 cd src
 python3 setup.py build_ext -lR -l:Rcpp.so -l:RcppArmadillo.so -L/usr/lib/R/site-library/Rcpp/libs/ -L/usr/lib/R/site-library/RcppArmadillo/libs/
+export LD_LIBRARY_PATH=/usr/lib/R/site-library/Rcpp/libs/:/usr/lib/R/site-library/RcppArmadillo/libs/
+# prepare to import the py-dynamic-lib after install
