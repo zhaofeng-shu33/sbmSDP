da03987a5f206345743771965d9ae1bb5a54e467
zhaofeng-shu33 <616545598@qq.com>
2020-09-15 22:08:08 +0800

    add libR.so path

--- src/setup.py
+++ src/setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import sys
 if sys.path[-1].find('conda') > 0:
-    root_dir = '/home/feng/.conda/envs/r_env/lib/R/library/'
-    r_dir = '/home/feng/.conda/envs/r_env/lib/R/include'
-else:
+    conda_root = '/home/feng/.conda/envs/r_env/'
+    root_dir = conda_root + 'lib/R/library/'
+    r_dir = conda_root + 'lib/R/include/'
+    r_so_dir = conda_root + 'lib/R/lib/'
+else: # debian buster
     root_dir = '/usr/lib/R/site-library/'
-    r_dir = '/usr/share/R/include'
+    r_dir = '/usr/share/R/include/'
+    r_so_dir = ''
 extensions = [
     Extension("sdp_admm_py", ["sdp_admm_py.pyx", "sdp_admm.cpp"],
         include_dirs=[root_dir + 'RcppArmadillo/include',
@@ -16,7 +19,7 @@ extensions = [
         libraries = ['R', 'RInside', ':Rcpp.so', ':RcppArmadillo.so'],
         library_dirs = [root_dir + 'Rcpp/libs/',
             root_dir + 'RcppArmadillo/libs/',
-            root_dir + 'RInside/lib/'])]
+            root_dir + 'RInside/lib/', r_so_dir])]
 setup(
     name='sdp_admm_py',
     ext_modules=cythonize(extensions),
